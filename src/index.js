e7f1b5a317afb2df299869cc074de8491758db6d
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-09 23:18:45 +0100

    Add header component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Header from "./app/layout/Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(<React.StrictMode><Header/></React.StrictMode>);
