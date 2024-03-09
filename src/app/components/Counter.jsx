b6170f2a2b18300ade7bbd58a1bd085567b7d235
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-10 00:05:27 +0100

    Display initial counter slice using selector redux

--- src/app/components/Counter.jsx
+++ src/app/components/Counter.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { UseSelector, useSelector } from 'react-redux'
 
 function Counter() {
+
+    const count = useSelector((state)=>state.counterStore.count);
+
   return (
-    <div>Counter</div>
+    <div>Counter: {count}</div>
   )
 }
 
