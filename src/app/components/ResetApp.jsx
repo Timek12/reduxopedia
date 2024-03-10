f0ddd8771090f40b53f36e36c7eb8c50300c4daa
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-10 22:23:59 +0100

    Add dispatch for both reset destination and reset counter actions

--- src/app/components/ResetApp.jsx
+++ src/app/components/ResetApp.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { resetCounter } from "../../redux/slice/counterSlice";
+import { resetDestination } from "../../redux/slice/destinationSlice";
+import { useDispatch } from "react-redux";
 
 function ResetApp() {
+  const dispatch = useDispatch();
+  const resetCounterAndDestination = () => {
+    dispatch(resetDestination());
+    dispatch(resetCounter());
+  };
+
   return (
     <div className="text-center">
-      <button className="btn btn-warning">Reset App</button>
+      <button
+        className="btn btn-warning"
+        onClick={resetCounterAndDestination}
+      >
+        Reset App
+      </button>
     </div>
   );
 }
