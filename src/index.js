b93829cc54edbc7c6fb3566a556e610d39ab3cda
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-10 22:08:43 +0100

    Add reset app component with reset app buton and append it to UI

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import { Provider } from "react-redux";
 import Counter from "./app/components/Counter";
 import DestinationList from "./app/components/DestinationList";
 import DestinationDetails from "./app/components/DestinationDetails";
+import ResetApp from "./app/components/ResetApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div className="text-white">
     <Provider store={store}>
       <Header />
+      <ResetApp/>
       <Counter />
       <div className="p-4 border">
         <h4 className="text-success text-center pb-2">Destination Lsit</h4>
